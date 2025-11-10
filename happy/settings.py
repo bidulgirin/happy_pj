cc078782166e39bb15212e6b25e2c4fc3d7106ce
bidulgirin <de05164@naver.com>
2025-11-10 23:45:40 +0900

    [fix]테스트시 다음 버튼 동작 오류 선택안했는데도 다음동작이되는 오류

--- happy/settings.py
+++ happy/settings.py
@@ -41,8 +41,8 @@ def get_secret(setting):
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
-DEBUG = True
+DEBUG = False
+#DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'de05164.pythonanywhere.com'
